9aa37ffcf17e3c9dc243a9a42c36bf3ed129f7ca
mariam <mariamrevia@gmail.com>
2022-12-15 12:53:55 +0400

    added useRef hook for removing shadow when clicking enter

--- src/components/inputField.tsx
+++ src/components/inputField.tsx
@@ -1,47 +1,44 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Interface } from 'readline'
 
 
 interface Props {
-    todo:string;
-    setTodo:React.Dispatch<React.SetStateAction<string>>;
-    handleAddedTask:(e:React.FormEvent) =>void
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAddedTask: (e: React.FormEvent) => void
+
 }
-
-
-
-
-const InputField = ({todo,setTodo,handleAddedTask}:Props) => {
-
+const InputField = ({ todo, setTodo, handleAddedTask }: Props) => {
+  let inputRef = useRef<HTMLInputElement>(null)
   return (
     <div className='inputFiledDiv'>
 
-    <form className='inputForm' onSubmit={handleAddedTask}>
-    <div className='div1'>
-    <input
-    type="text" 
-    placeholder='   Enter a text' 
-    className="taskInput"
-    value={todo}
-    onChange={ (e) =>
-      setTodo(e.target.value)
-    }
-    >
-    </input>
+      <form className='inputForm' onSubmit={
+        (e) => {
+          handleAddedTask(e)
+          inputRef.current?.blur()
+        }
+      }>
 
-    <button
-    type="submit" 
-    className='goBtn'
-    >GO</button>
-    
-    </div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder='   Enter a text'
+          className="taskInput"
+          value={todo}
+          onChange={(e) =>
+            setTodo(e.target.value)
+          }
+        >
+        </input>
+
+        <button
+          type="submit"
+          className='goBtn'
+        >GO</button>
 
 
-
-
-    </form>
-
-
+      </form>
     </div>
   )
 }
